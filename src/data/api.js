437c13f264ec34d5ebf3e9ac4cf91203aa2074e4
sunny-potato <sun_hee0210@naver.com>
2022-12-02 10:51:07 +0100

    fixed ngrok

--- src/data/api.js
+++ src/data/api.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 
 const baseURL = 'https://6230-84-209-59-19.eu.ngrok.io'; // when ngrok used
 
-// const baseURL = 'http://192.168.0.10:3000'; // when IPv4 address used (IPv4 Address:3000)
-
 export const useApi = path => {
   const [data, setData] = useState();
 
